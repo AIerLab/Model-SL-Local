fa52f0092a096f93875308cc581633dfa5cbfb75
Haobo Yang <hobart.yang@qq.com>
2023-07-18 16:43:42 +0800

    update expose server following the api doc

---
+++ src/helper/shared.py
@@ -0,0 +1,8 @@
+# Global variable
+# This dictionary stores all the roles, chats, current role and chat UID.
+DATA = dict(
+    roles=[],
+    chats=dict(),
+    current_role_uid=None,
+    current_chat_uid=None
+)
--- src/socket_main.py
+++ src/socket_main.py
@@ -17,29 +17,29 @@ if __name__ == '__main__':
     # CLIENT_DIR = "../tmp/client/c01"
 
     # model_layers = nn.ModuleList([])
-    model_layers = nn.ModuleList([nn.Identity() for layer_id in range(1)]) # TODO change the number of split layers
+    model_layers = nn.ModuleList([nn.Identity() for layer_id in range(0)]) # TODO change the number of split layers
     # model_layers = nn.ModuleList([nn.Linear(in_features=4096, out_features=4096, bias=True) for layer_id in range(4)])
 
     # Init data, socket and model.
-    client = SplitSocket(socket.gethostbyname("server.natappfree.cc"), 41837)
+    client = SplitSocket(socket.gethostbyname("server.natappfree.cc"), 46519)
     model = SplitClientModel(model_layers, client, CLIENT_DIR).half()
 
-    print("Welcome to the ChatGLM-6B model. Type your message.")
-    while True:
-        try:
-            query = input("\nUser: ").strip()
-            if query == "stop":
-                model.stop() # TODO
-                break
-            elif query == "clear":
-                model.clear() # TODO
-                print("Chat history cleared.")
-            else:
-                response = model.process(query)
-                print(response)
-        except:
-            print("Error")
+    # print("Welcome to the ChatGLM-6B model. Type your message.")
+    # while True:
+    #     try:
+    #         query = input("\nUser: ").strip()
+    #         if query == "stop":
+    #             model.stop() # TODO
+    #             break
+    #         elif query == "clear":
+    #             model.clear() # TODO
+    #             print("Chat history cleared.")
+    #         else:
+    #             response = model.process(query)
+    #             print(response)
+    #     except:
+    #         print("Error")
 
     # FIXME for server use
-    # server = Server(model.process)
-    # server.run("localhost", 10086)
+    server = Server(model.process)
+    server.run("localhost", 8080)
